fbb75c96c0757b1b6565ce3b539bf1901570bd0c
sarthaksanyasi <sanyasisarthak@gmail.com>
2023-04-02 18:44:39 -0400

    new html code

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const labelRadio = document.getElementsByName("label-group");
 const csvFileInput = document.getElementById("csv-file");
 const csvDropdown = document.getElementById("csv-dropdown");
 
-function toggleDropdown() {
-    if (document.getElementById("label1").checked) {
-        csvDropdown.style.display = "block";
-    } else {
-        csvDropdown.style.display = "none";
-    }
+// Hide the dropdown initially
+csvDropdown.style.display = "none";
+
+// Listen for changes to the radio buttons
+for (let i = 0; i < labelRadio.length; i++) {
+    labelRadio[i].addEventListener("change", (event) => {
+        if (event.target.value === "label1") {
+            csvDropdown.style.display = "block";
+        } else {
+            csvDropdown.style.display = "none";
+        }
+    });
 }
 
 csvFileInput.addEventListener("change", (event) => {
@@ -27,31 +34,3 @@ csvFileInput.addEventListener("change", (event) => {
 
     reader.readAsText(file);
 });
-
-
-/* old code start
-
-const csvFileInput = document.getElementById("csv-file");
-const csvDropdown = document.getElementById("csv-dropdown");
-
-csvFileInput.addEventListener("change", (event) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-
-  reader.onload = (event) => {
-    const csv = event.target.result;
-    const rows = csv.split("\n");
-    const headers = rows[0].split(",");
-
-    headers.forEach((header) => {
-      const option = document.createElement("option");
-      option.textContent = header;
-      csvDropdown.appendChild(option);
-    });
-  };
-
-  reader.readAsText(file);
-});
-
-
-old code end*/
