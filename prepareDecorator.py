7485c6b1062535cb436d6fb22f812c1440e0f870
Rajat Gedam <rajat.gedam96@gmail.com>
2023-04-09 17:42:53 -0400

    Updated prepareDecorator ms

--- prepareDecorator.py
+++ prepareDecorator.py
@@ -106,6 +106,7 @@ class ImputeData(prepDecorator):
 		
 		return idf
 
+
 #Main
 
 
@@ -126,68 +127,68 @@ prepdata(df,labelCol,nanThreshold)
 #app = Flask(__name__)
 
 #@app.route('/prepdata', methods=['POST'])
-def prepdata(file, labelCol, nanThreshold):
+#def prepdata(file, labelCol, nanThreshold):
 
-	#initialize variables
-	#nanThreshold = 0.6
-	enc = OrdinalEncoder()
-	le = LabelEncoder()
-	imputer = SimpleImputer(strategy="mean")
+#	#initialize variables
+#	#nanThreshold = 0.6
+#	enc = OrdinalEncoder()
+#	le = LabelEncoder()
+#	imputer = SimpleImputer(strategy="mean")
 
-	df=file.copy()
-	df2=file.copy()
+#	df=file.copy()
+#	df2=file.copy()
 
-	labelCol_cat=labelCol+'_cat'
+#	labelCol_cat=labelCol+'_cat'
 
-	#Label categorical data
-	#get label from user 
+#	#Label categorical data
+#	#get label from user 
 
-	#df[labelCol] = enc.fit_transform(df[[labelCol]])
-	df2[labelCol] = le.fit_transform(df2[labelCol])
+#	#df[labelCol] = enc.fit_transform(df[[labelCol]])
+#	df2[labelCol] = le.fit_transform(df2[labelCol])
 
-	#ordinal encoder
+#	#ordinal encoder
 
-	#print(enc.categories_)
-	#print(df2.shape)
+#	#print(enc.categories_)
+#	#print(df2.shape)
 	
-	#Replace ? with NaN
-	df2 = df2.replace("?", np.nan)
-	print(df2)
-	print(df2.shape)
-	#Check the % of NaN in each column
-	x=df2.isna().sum()/len(df2)*100
-	#print(x[2])
-	#print(type(x))
+#	#Replace ? with NaN
+#	df2 = df2.replace("?", np.nan)
+#	print(df2)
+#	print(df2.shape)
+#	#Check the % of NaN in each column
+#	x=df2.isna().sum()/len(df2)*100
+#	#print(x[2])
+#	#print(type(x))
 	
-	#Drop the columns with more than x % NaN values
-	df2.dropna(thresh=nanThreshold*len(df2),axis=1,inplace=True)
+#	#Drop the columns with more than x % NaN values
+#	df2.dropna(thresh=nanThreshold*len(df2),axis=1,inplace=True)
 
-	#print(df2)
+#	#print(df2)
 
-	#Imputation
-	#Encountered error before adding categorical encoding
+#	#Imputation
+#	#Encountered error before adding categorical encoding
 
-	imputer.fit(df2)
-	imputedDF = imputer.transform(df2)
+#	imputer.fit(df2)
+#	imputedDF = imputer.transform(df2)
 
-	#print(imputedDF)
-	#print(pd.DataFrame(imputedDF))
+#	#print(imputedDF)
+#	#print(pd.DataFrame(imputedDF))
 
-	idf = pd.DataFrame(imputedDF)
+#	idf = pd.DataFrame(imputedDF)
 
-	print(idf)
+#	print(idf)
 
-	return 0
+#	return 0
 
-#Main
+##Main
 
 
-labelCol = ''
+#labelCol = ''
 
 	
-labelCol = 'diagnosis'
-nanThreshold = 0.6
-#Read file
-df = pd.read_csv('breast-cancer.csv')
+#labelCol = 'diagnosis'
+#nanThreshold = 0.6
+##Read file
+#df = pd.read_csv('breast-cancer.csv')
 
-prepdata(df,labelCol,nanThreshold)
+#prepdata(df,labelCol,nanThreshold)
