38b4211820bb40a55567a43889f89125b55aa4cc
Rajat Gedam <rajat.gedam96@gmail.com>
2023-04-09 20:03:35 -0400

    Updated input service, changed html - Sarthak

--- input.py
+++ input.py
@@ -1,7 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request,send_file, render_template, jsonify
 import pandas as pd
 import json as js
-import csv as csvtest
 from io import StringIO
 from werkzeug.utils import secure_filename
 
@@ -9,55 +8,29 @@ app = Flask(__name__)
 
 @app.route('/csv_upload', methods=['POST'])
 def csv_upload():
-    # Get the uploaded file from the request
-    
-    ###### intial logic for printing headers start ######
-    #csv_file = pd.read_csv(request.files['file'], index_col=0, nrows=0).columns.tolist()
-    
-    #print('headers were: ', csv_file)
-    #y = '\n'.join(csv_file)
-    ###### intial logic for printing headers end ########
-
-    # Load the CSV file into a pandas DataFrame
-    #df = pd.read_csv(csv_file)
-
-    #testcols = pd.read_csv(StringIO(request.files['file']), index_col=0, nrows=0).columns.tolist()
-
-    #csv_file = request.files['file']
-    
-    #csv_file = pd.read_csv(request.files['file'])
-    #file_data = csv_file.read().decode("utf-8")
-    
-    #with open(file_data, newline='') as file:
-    #    reader = csvtest.reader(file, delimiter = ' ')
-    #    headings = next(reader)
-    
-    ## Perform some operation on the DataFrame
-    ## For example, let's calculate the sum of the first column
-    #sum_first_col = df.iloc[:, 0].sum()
-    
-    #print('result is ', sum_first_col)
-    ## Return the result
-    #y = js.dumps(int(sum_first_col))
-
-    #test comment
-    #return {'sum_first_col': sum_first_col}
-    #return y
-    
-    # Code Changes for safe file :: Start : Sarthak ###########################
-
+    # Get the uploaded file from the frontend
     file = request.files['file']
-    filename = secure_filename(file.filename)
-    # Check if the filename is safe
-    if filename != file.filename:
-        return 'Unsafe filename', 400
-    # Process the file
-    # ...
-    return 'File uploaded successfully'
 
-    # Code Changes for safe file :: End : Sarthak #############################
+    # Read the first line of the CSV file to get the header row
+    #header = file.readline().decode().strip()
 
+    # Get the name of the first column
+    #first_column = header.split(',')[0]
     
+    selected_column = request.form['csv_dropdown']
+    
+    df = pd.read_csv(file)
+
+    # Return the name of the first column as a plain text response
+    #return first_column
+    
+    df.to_csv('../cis600_project/uploaded_CSV_file/process_data.csv', encoding='utf-8', index=False)
+
+    return jsonify({
+        #'csv': df.to_dict(),
+        #'first_column': first_column,
+        'selected_column': selected_column
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
